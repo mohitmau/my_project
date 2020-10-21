21e3e717fd670a200030acaed5260a4a9d82c743
Mohit <maumhr12@gmail.com>
2020-10-21 08:48:25 +0545

    axios example added

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('hello world');
+const axios = require('axios');
+
+axios.get('https://5f8fa16c693e730016d7b35f.mockapi.io/api/users')
+  .then(function (response) {
+    // handle success
+    response.data.forEach(function(value,index){
+    	console.log(value.name);
+    })
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
